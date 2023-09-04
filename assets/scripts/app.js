c9c27b3c0284a7d675c4327e6f4ff082a46781a9
lumi <fatlum.llumnica@gmail.com>
2023-09-04 11:11:06 +0200

    if statement in use

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,6 +30,16 @@ function writeToLog(
 
 function calculateResult(calculationType) {
   const enteredNumber = getUserNumberInput();
+  if (
+    (calculationType !== "ADD" &&
+      calculationType !== "SUBTRACT" &&
+      calculationType !== "MULTIPLY" &&
+      calculationType !== "DIVIDE") ||
+    !enteredNumber
+  ) {
+    return;
+  }
+
   // const calcDescription =
   //   currentResult === 0
   //     ? (currentResult === 0 ? " " : currentResult + " + ") +
